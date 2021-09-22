791ab592809e69b62fc9725037336e2cb3b92e37
AlfredoRoque <46386649+AlfredoRoque@users.noreply.github.com>
2021-09-21 20:58:34 -0500

    version 1.0

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/pokedex'));
 app.listen(process.env.PORT || 8080);
 app.get('/*', function(req,res){
-    res.sendFile(path.join('/dist/index.html'))
+    res.sendFile(path.join(__dirname + '/dist/pokedex/index.html'))
 });
 
 console.log("Listening!!");
